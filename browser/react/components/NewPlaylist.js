1000b3a09bfd8d0de85195f01db800e7cbbf97b1
Sam Bakkila <sbakkila@gmail.com>
2017-03-30 12:02:26 -0400

    form validation works

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const NewPlaylist = () => {
+const NewPlaylist = (props) => {
   return (
     <div className="well">
-      <form className="form-horizontal">
+      <form onSubmit={props.onSubmit}  className="form-horizontal">
         <fieldset>
           <legend>New Playlist</legend>
           <div className="form-group">
             <label className="col-xs-2 control-label">Name</label>
             <div className="col-xs-10">
-              <input className="form-control" type="text"/>
+              <input className="form-control" onChange={props.onChange} value={props.initialValue} type="text"/>
             </div>
           </div>
           <div className="form-group">
             <div className="col-xs-10 col-xs-offset-2">
-              <button type="submit" className="btn btn-success">Create Playlist</button>
+              <button disabled={props.validation()} type="submit" className="btn btn-success" >Create Playlist</button>
             </div>
           </div>
         </fieldset>
--- browser/react/containers/NewPlaylistContainer.js
+++ browser/react/containers/NewPlaylistContainer.js
@@ -1,2 +1,42 @@
 import React from 'react';
-import NewPlaylist from '../components/NewPlaylist'
+import NewPlaylist from '../components/NewPlaylist';
+import axios from 'axios';
+
+export default class NewPlaylistContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playlist: ''
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.validateInput = this.validateInput.bind(this);
+  }
+
+  validateInput(){
+    return !(this.state.playlist.length > 0 && this.state.playlist.length < 17);
+  }
+
+  onChange(event){
+    this.setState({playlist: event.target.value})
+    console.log(this.state.playlist);
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+
+    axios.post('/api/playlists/', {name: this.state.playlist})
+    .then( (res) => res.data)
+    .then ( (newPlaylist) => {
+      this.setState({playlist: ''});
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        < NewPlaylist onSubmit={this.onSubmit} onChange={this.onChange} initialValue={this.state.playlist} validation={this.validateInput}/>
+      </div>
+    )
+  }
+}
